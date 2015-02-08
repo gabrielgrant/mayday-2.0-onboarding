6eb3cc36be4b263f928af7d0c80063b35e5844d1
Gabriel Grant <g@briel.ca>
2015-02-08 13:42:59 -0500

    create zip capture step

---
+++ app/controllers/index.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  needs: ['application'],
+  actions: {
+    next: function(){
+      if (this.get('name')){
+        this.set('controllers.application.name', this.get('name'));
+      }
+      if (this.get('email')){
+        this.set('controllers.application.email', this.get('email'));
+      }
+      this.set('controllers.application.zip', this.get('zip'));
+      var url = 'https://congress.api.sunlightfoundation.com/districts/locate';
+      var data = {
+        apikey: '65b4c5e0e363435790291c0c76067cce',
+        zip: this.get('zip')
+      }
+      var self = this;
+      $.getJSON(url, data).then(function(data){
+        // if no 
+        //if (data.length == 1
+        console.log(data);
+        self.transitionToRoute('call');
+      });
+      
+    }
+  }
+});
