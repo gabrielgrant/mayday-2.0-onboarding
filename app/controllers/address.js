a1b0b5eaf7d1c5aac68d5283a32c4fcd7a4705da
Gabriel Grant <g@briel.ca>
2015-03-04 21:04:42 -0800

    properly capture address and redirect to call

--- app/controllers/address.js
+++ app/controllers/address.js
@@ -3,7 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     next: function(){
-      console.log('save address');
+      var address = this.get('address');
+      var model = this.get('model');
+      Ember.set(model, 'address', address)
+      console.log('captured address', model);
       this.transitionToRoute('call');
     }
   }
