435c9d126c6799a746960e23ca3f569958bdca14
Gabriel Grant <g@briel.ca>
2015-03-04 21:05:13 -0800

    initiate calls

--- app/controllers/call/index.js
+++ app/controllers/call/index.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['application'],
   actions: {
     next: function(){
-      console.log('init call');
+      console.log('init call model', this.get('model'));
+      var url = 'https://mayone-staging.herokuapp.com/calls';
+      var data = {
+        email: this.get('controllers.application.model.email'),
+        phone: this.get('phone')
+      };
+      Ember.$.post(url, data);
       this.transitionToRoute('call.script');
     }
   }
